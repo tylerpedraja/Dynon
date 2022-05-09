fc620fe6e4ea32b93fbb22a2b7e8d40bf97b9f62
Tyler Pedraja <tpedraja@tylerpedraja.com>
2022-05-09 13:23:16 -0400

    debugging

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     )
   } else {
-    app.get('/', (req, res) => {
+    app.get('/api', (req, res) => {
       res.send('API is running....')
     })
   }
