138e35c254e87f5185eca3984c30920a27570448
Tyler Pedraja <tpedraja@tylerpedraja.com>
2022-05-22 15:54:40 -0400

    Making endpoints better

--- frontend/src/components/WorksheetComponents/ProductsWorksheetSection.js
+++ frontend/src/components/WorksheetComponents/ProductsWorksheetSection.js
@@ -6,7 +6,79 @@ import ProductWorksheetItemHeader from '../WorksheetComponents/ProductWorksheetI
 const ProductsWorksheetSection = (props) => {
     const [renderable, setRenderable] = useState(false)
 
+    const renderProductTypeContent = () => {
+        if (props.products.length === 0) return
+        return (
+            <div className="pb-3">
+                <ProductWorksheetItemHeader
+                    type={
+                        props.type
+                    }
+                    header={
+                        props.header
+                    }
+                    subheader={
+                        props.subheader
+                    } />
+                <table className="table">
+                    {
+                        props.subtypes.map((subtype, index) => {
+                            const productSubtypes = [];
+                            props.products.forEach(product => {
+                                productSubtypes.push(product.subgroup)
+                            })
+                            if (productSubtypes.includes(subtype.name)) return (
+                                <React.Fragment key={index} >
+                                    <thead className="text-white rounded"
+                                        style={
+                                            { backgroundColor: '#212529' }
+                                        }>
+                                        <tr>
+                                            <th scope="col" className="d-none d-md-table-cell">Part Number</th>
+                                            <th scope="col">
+                                                {
+                                                    subtype.name
+                                                }</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Adjust</th>
+                                            <th scope="col">Qty.</th>
+                                            <th scope="col" width="100px">Subtotal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {props.products.map((product, index) => {
+                                            if (product.subgroup.toLowerCase() == subtype.name.toLowerCase()) {
+                                                return (
+                                                    <ProductWorksheetItem
+                                                        key={
+                                                            index
+                                                        }
+                                                        part_number={
+                                                            product.part_number
+                                                        }
+                                                        name={
+                                                            product.name
+                                                        }
+                                                        price={
+                                                            product.price
+                                                        }
+                                                        qty_in_stock={
+                                                            product.qty_in_stock
+                                                        } />
+                                                )
+                                            }
+                                        })}
+                                    </tbody>
+                                </React.Fragment>
+                            )
+                        })
+                    }</table>
+            </div>
+        )
+    }
+
     useEffect(() => {
+        console.log(props)
         if (props.subtypes.length > 0) {
             setRenderable(true)
         }
@@ -16,67 +88,7 @@ const ProductsWorksheetSection = (props) => {
     return (
         <> {
             renderable && (
-                <div className="pb-3">
-                    <ProductWorksheetItemHeader
-                        type={
-                            props.type
-                        }
-                        header={
-                            props.header
-                        }
-                        subheader={
-                            props.subheader
-                        } />
-                    <table className="table">
-                        {
-                            props.subtypes.map((subtype, index) => {
-                                return (
-                                    <React.Fragment key={index} >
-                                        <thead className="text-white rounded"
-                                            style={
-                                                { backgroundColor: '#212529' }
-                                            }>
-                                            <tr>
-                                                <th scope="col" className="d-none d-md-table-cell">Part Number</th>
-                                                <th scope="col">
-                                                    {
-                                                        subtype.name
-                                                    }</th>
-                                                <th scope="col">Price</th>
-                                                <th scope="col">Adjust</th>
-                                                <th scope="col">Qty.</th>
-                                                <th scope="col" width="100px">Subtotal</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {props.products.map((product, index) => {
-                                                if (product.subgroup.toLowerCase() == subtype.name.toLowerCase()) {
-                                                    return (
-                                                        <ProductWorksheetItem
-                                                            key={
-                                                                index
-                                                            }
-                                                            part_number={
-                                                                product.part_number
-                                                            }
-                                                            name={
-                                                                product.name
-                                                            }
-                                                            price={
-                                                                product.price
-                                                            }
-                                                            qty_in_stock={
-                                                                product.qty_in_stock
-                                                            } />
-                                                    )
-                                                }
-                                            })}
-                                        </tbody>
-                                    </React.Fragment>
-                                )
-                            })
-                        }</table>
-                </div>
+                renderProductTypeContent()
             )
         } </>
     )
